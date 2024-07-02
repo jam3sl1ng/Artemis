ba7ef076f13c9c0227cb436e27f738a41bd5333b
jam3sl1ng <jamesling596@gmail.com>
2024-07-02 17:12:57 +0100

    init

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Hello World")
+# import tensorflow as tf
+# import numpy
+# import nltk
+# import pytorch
+# import pyttsx3 # Optional (needed for tts)
+# import huggingface
+
+user_input = input("what is your query? ")
+ai_output = 0
+
+if user_input == str("hello"):
+    ai_output = ("hi how are you?")
+    print(ai_output)
